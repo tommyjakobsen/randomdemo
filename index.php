fbab92f385e4e7a5739e0efe2af41f5f7e22fd83
Tommy Jakobsen <tommy.jakobsen@evry.com>
2019-05-27 07:55:22 +0000

    debug

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
-<body>
-Halla balla
-</body>
+<head>
+<style>
+body { 
+  background-image: url('w3css.gif');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center; 
+  background-size: auto;
+}
+</style>
+</head>
 </html>
