44a441720412d8ff8064c234d213841b41376775
Tommy Jakobsen <tommy.jakobsen@evry.com>
2019-05-27 13:34:09 +0000

    debug

--- img.php
+++ img.php
@@ -1,5 +1,22 @@
 <?php
 
+
+if(getenv('background') !== false)
+    {
+        $bgcolor=getenv('background');
+     }else{
+        $bgcolor="blue";
+     }
+
+if(getenv('title') !== false)
+    {
+        $title=getenv('title');
+     }else{
+        $title="My little place...";
+     }
+
+
+
 function LoadPNG($imgname)
 {
     /* Attempt to open */
@@ -42,6 +59,9 @@ imagefttext($img, 13, 0, 50, 530, $red, $font_file, "3\nUse values from\nAnsible
 imageline($img, 200, 570, 520, 570, $green);
 imagefttext($img, 13, 0, 330, 530, $red, $font_file, "4\nDeploy");
 
+//APP LINE
+imageline($img, 450, 80, 580, 230, $red);
+
 //Get git code
 imageline($img, 700, 600, 800, 100, $green);
 imagefttext($img, 13, 0, 700, 130, $red, $font_file, "5\nGet Code\nfrom SCM");
@@ -55,9 +75,27 @@ $container = imagecreatefrompng('img/small_container.png');
 $container = imagescale($container, 100);
 imagesetthickness($img, 3);
 imagesetbrush($img, $container);
- 
-imageline($img, 100, 50, 200, 200, IMG_COLOR_BRUSHED);
 
+imageline($img, 880, 500, 880, 500, IMG_COLOR_BRUSHED);
+imageline($img, 700, 500, 700, 500, IMG_COLOR_BRUSHED);
+imageline($img, 880, 500, 880, 500, IMG_COLOR_BRUSHED);
+
+imagefttext($img, 13, 0, 620, 380, $red, $font_file, "7\nContainers\nRunning");
+
+
+//INFRA
+imagefttext($img, 13, 0, 820, 220, $red, $font_file, "8\nApp Exposed\nthrough Infra");
+
+
+//THE PAGE
+$cloud = imagecreatefrompng("./img/${bgcolor}_cloud.png");
+$cloud = imagescale($cloud, 150);
+imagesetthickness($img, 3);
+imagesetbrush($img, $cloud);
+
+imageline($img, 600, 50, 600, 80, IMG_COLOR_BRUSHED);
+
+imagefttext($img, 13, 0, 550, 80, $black, $font_file, "8\n$title");
 
 
 imageantialias($img, true);
