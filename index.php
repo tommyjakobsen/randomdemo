b71d5bebb6713098936670afe6551000bb618cff
Tommy Jakobsen <tommy.jakobsen@evry.com>
2019-05-27 07:59:59 +0000

    debug

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <html>
 <head>
 <style>
+<?php
+
+echo "
 body { 
-  background-image: url('w3css.gif');
+  background-image: url('./img/blue_cloud.png');
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center; 
   background-size: auto;
 }
+";
+
+?>
 </style>
 </head>
 </html>
